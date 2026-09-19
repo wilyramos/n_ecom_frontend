138edbef43cea23d3945e6a8c89dd53fcac23a0a
wilyramos <wily.ramos1@unmsm.edu.pe>
2026-09-19 00:18:32 -0500

    fix s

--- components/home/ModalPopupAd.tsx
+++ components/home/ModalPopupAd.tsx
@@ -57,7 +57,7 @@ export default function ModalPopupAd({ ad }: ModalPopupAdProps) {
                         fill
                         className="object-cover"
                         priority
-                        quality={100}
+                        quality={80}
                     />
                 </div>
             )}

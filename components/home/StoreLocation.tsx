9253588764aeff6e24280439b641c1d0c34dcd25
wilyramos <wily.ramos1@unmsm.edu.pe>
2026-09-19 12:39:00 -0500

    fix err

--- components/home/StoreLocation.tsx
+++ components/home/StoreLocation.tsx
@@ -115,13 +115,13 @@ export default function StoreLocation() {
 
             {/* MAPA */}
             <div className="flex-1 overflow-hidden rounded-md border border-brand-silver-border">
-              <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3900.71171804938!2d-76.9945115!3d-12.1318464!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105b8178cd39e33%3A0x633512e09153574c!2sAv.%20Caminos%20del%20Inca%20257%2C%20Santiago%20de%20Surco%2015038!5e0!3m2!1ses-419!2spe!4v1717684000000!5m2!1ses-419!2spe"
-                className="h-full w-full border-0"
+  <iframe
+    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d975.253689409893!2d-76.9924689!3d-12.1138673!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9105c7e5102df947%3A0x3c711f9098ede003!2sNeoshop%20Importaciones!5e0!3m2!1ses-419!2spe!4v1710864000000!5m2!1ses-419!2spe"
+    className="h-full w-full border-0"
     loading="lazy"
     referrerPolicy="no-referrer-when-downgrade"
-/>
-            </div>
+  />
+</div>
           </div>
 
           {/* COLUMNA DERECHA */}
